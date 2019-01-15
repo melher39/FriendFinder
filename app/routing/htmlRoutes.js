2f8a5c47f9fc4b8d53a5966d9cc83ace6187b0f6
Melvin Hernandez <melher39@gmail.com>
2019-01-14 21:03:52 -0600

    htmlRoute.js file updated. will now export routing code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,12 +5,19 @@
 // path package is needed to get the correct file paths for the html file
 const path = require("path");
 
+// exporting the following code to be later imported by server.js
+// followed the example in week13-activity16-solved
+module.exports = (app) => { 
 
+// if the path is /survey, then the corresponding html page will be loaded
 app.get("/survey", (req, res) => {
-    res.sendFile(path.join(__dirname, "survey.html"))
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "home.html"))
+// * means all/any other route not matching the above will default to home
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
 });
+
+};
 
